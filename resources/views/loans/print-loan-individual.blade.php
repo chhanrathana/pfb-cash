cf45423d65f018769cad791a6a15b0dfc9e27cae
Chhan Rathana <chhrathana@gmail.com>
2022-12-25 17:30:49 +0700

    fixed conflit

--- resources/views/loans/print-loan-individual.blade.php
+++ resources/views/loans/print-loan-individual.blade.php
@@ -63,14 +63,16 @@
     </div>
     
     <div class="text-right">
-        <small class="print-date"><i>printed at {{ \Carbon\Carbon::now() }}</i></small>
+        <small class="print-date"><i>printed at{{ \Carbon\Carbon::now() }}</i></small>
     </div>
+    
+
     <div class="report" id="report">
         <table class="th-color">
             <tr>
                 <th style="width: 10%; font-size: smaller; padding: 4px;">ល.រ</th>
                 <th colspan="2" style="width: 30%; font-size: smaller; padding: 4px;">កាលបរិច្ឆេទសងប្រាក់</th>
-                <th style="width: 30%; font-size: smaller; padding: 4px;">ចំនួនទឹកប្រាក់សងមួយលើកៗ</th>
+                <th style="width: 10%; font-size: smaller; padding: 4px;">ទឹកប្រាក់ត្រូវបង់</th>
                 <th style="width: 220px; font-size: smaller; padding: 4px;">សម្គាល់ផ្សេងៗ</th>
             </tr>
             @foreach ($loan->payments as $payment)
@@ -78,20 +80,22 @@
                     <td style="font-size: smaller; padding: 4px" class="text-center text-nowrap">{{ $loop->index + 1 }}</td>
                     <td style="font-size: smaller; padding: 4px" class="text-center" >{{ $payment->payment_date??''}} </td>
                     <td style="font-size: smaller; padding: 4px" class="text-center" nowrap="nowrap">{{ convertDaytoKhmer(date('D',strtotime($payment->getRawOriginal('payment_date')))) }}</td>
-                    <td style="font-size: smaller; padding: 4px" class="text-right text-nowrap"></td>
+                    <td style="font-size: smaller; padding: 4px" class="text-right text-nowrap">{{ number_format($payment->total_amount) }}</td>                    
                     <td style="font-size: smaller; padding: 4px" class="text-right text-nowrap"></td>
                 </tr>
             @endforeach
-        </table>
-        <p style="  text-indent: 30px; font-size:12px">ក្រោយពីបានពិនិត្យកិច្ចសន្យានិងតារាងបង់ប្រាក់ខាងលើ ខ្ញុំបាទ/នាងខ្ញុំបានយល់ព្រមបង់សងប្រាក់បងស្ថាប័នវិញទាំងប្រាក់ដើមនិងការប្រាក់ទៅតាមតារាងបង់ប្រាក់តាមការកំណត់ខាងលើជាកំហិត។</p>
-    </div>
-    <table style="width: 100%; font-size:10px" class="table-non-border td-border-non line-height-2">
+            
+        </table>                
+    </div>    
+    <p style="text-indent: 10px; font-size:12px">ក្រោយពីបានពិនិត្យកិច្ចសន្យានិងតារាងបង់ប្រាក់ខាងលើ ខ្ញុំបាទ/នាងខ្ញុំបានយល់ព្រមបង់សងប្រាក់បងស្ថាប័នវិញទាំងប្រាក់ដើមនិងការប្រាក់ទៅតាមតារាងបង់ប្រាក់តាមការកំណត់ខាងលើជាកំហិត។</p>
+
+    <table style="width: 100%; font-size:10px" class="table-non-border td-border-non line-height-2">        
         <tr>
             <td>ហត្ថលេខាបុគ្គលិក</td>
             <td>ស្នាមមេដៃអ្នករួមធានា</td>
             <td>ស្នាមមេដៃអ្នកធានា</td>
             <td>ស្នាមមេដៃអ្នករួមខ្ចី</td>
-            <td>ស្នាមមេដៃអ្នកខ្ចី</td>
+            <td>ស្នាមមេដៃអ្នកខ្ចី</td>            
         </tr>
-    </table>
+    </table>    
 @stop
