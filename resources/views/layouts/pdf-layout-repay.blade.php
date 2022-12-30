d9cf3c1eb72949f20fa3f12c2b2f1524e1ea16de
Chhan Rathana <chhrathana@gmail.com>
2022-12-30 11:47:22 +0700

    memories limit

--- resources/views/layouts/pdf-layout-repay.blade.php
+++ resources/views/layouts/pdf-layout-repay.blade.php
@@ -1,3 +1,6 @@
+@php
+    ini_set('memory_limit', '-1');
+@endphp
 <!DOCTYPE html>
 <html>
   <head>
--- resources/views/layouts/pdf-layout.blade.php
+++ resources/views/layouts/pdf-layout.blade.php
@@ -1,3 +1,7 @@
+@php
+    ini_set('memory_limit', '-1');
+@endphp
+
 <!DOCTYPE html>
 <html>
   <head>
