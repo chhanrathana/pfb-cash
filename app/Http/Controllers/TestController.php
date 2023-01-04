c98575f91fc13edc9303923ac44cea7a48187f4c
buth <chhanvirakbuth1999@gmail.com>
2023-01-04 16:27:30 +0700

    fix isse duplicate

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Notification;
 
@@ -12,6 +13,7 @@ class TestController extends Controller
 {
     //
     public function any(Request $req){
-
+        $res = DB::table('clients')->max('code');
+       dd($res+1);
     }
 }
--- app/Http/Services/ClientService.php
+++ app/Http/Services/ClientService.php
@@ -22,8 +22,9 @@ class ClientService
 //        $count = auth()->user()->branch_id ? DB::table('clients')
 //        ->where('branch_id', auth()->user()->branch_id)
 //        ->count(): Client::count();
-        $count= DB::table('clients')->count();
-        $code = (168 + $count + 1);
+        $count= DB::table('clients')->max('code');
+//        $code = (168 + $count + 1);
+        $code = $count +1;
         return str_pad($code, 5, '0', STR_PAD_LEFT);
     }
 
