7cdf0d72349f4bd3a813d7478e6d9c900f5c5322
Chhan Rathana <chhrathana@gmail.com>
2023-01-15 11:38:09 +0700

    length

--- database/migrations/2021_01_14_145643_create_document_type_table.php
+++ database/migrations/2021_01_14_145643_create_document_type_table.php
@@ -14,7 +14,7 @@ class CreateDocumentTypeTable extends Migration
     public function up()
     {
         Schema::create('document_type', function (Blueprint $table) {
-            $table->string('id')->primary();
+            $table->string('id',10)->primary();
             $table->string('name',50)->nullable();
         });
     }
